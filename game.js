3ab87247991605da7644893c5c29ac616ba85f4a
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-08 13:44:14 -0500

    add btn click handler function to add userchosen colors to userclickedpattern array

--- game.js
+++ game.js
@@ -2,13 +2,24 @@ $(document).ready(function () {
   const buttonColors = ["red", "blue", "green", "yellow"];
 
   const gamePattern = [];
+  const userClickedPattern = [];
 
   nextSequence();
-
   function nextSequence() {
     const randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColor = buttonColors[randomNumber];
+    console.log(randomChosenColor);
     gamePattern.push(randomChosenColor);
-    console.log(gamePattern);
+    //flash
+    $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+    //audio
+    const audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+    // audio.play();
   }
+
+  $(".btn").click(function () {
+    const userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+  });
 });
