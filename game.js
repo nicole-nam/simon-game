132d5f78cb7f2c66fe82019d3992e4402696f201
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-08 14:10:47 -0500

    add button canimation to add and remove grey background on click

--- game.js
+++ game.js
@@ -11,17 +11,25 @@ $(document).ready(function () {
     gamePattern.push(randomChosenColor);
     //flash
     $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColor);
+    // playSound(randomChosenColor);
   }
 
   $(".btn").click(function () {
     const userChosenColor = this.id;
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
+    animatePress(userChosenColor);
   });
 
   function playSound(name) {
     const audio = new Audio(`sounds/${name}.mp3`);
     audio.play();
+  }
+
+  function animatePress(currentColor) {
+    $(`#${currentColor}`).addClass("pressed");
+    setTimeout(() => {
+      $(`#${currentColor}`).removeClass("pressed");
+    }, 100);
   }
 });
