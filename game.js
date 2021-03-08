368cf33191f03795a19aac8ccdd80456b8509ffd
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-08 13:45:27 -0500

    add sounds to the userclicks according to colors

--- game.js
+++ game.js
@@ -8,18 +8,19 @@ $(document).ready(function () {
   function nextSequence() {
     const randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColor = buttonColors[randomNumber];
-    console.log(randomChosenColor);
     gamePattern.push(randomChosenColor);
     //flash
     $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
     //audio
     const audio = new Audio(`sounds/${randomChosenColor}.mp3`);
-    // audio.play();
+    audio.play();
   }
 
   $(".btn").click(function () {
     const userChosenColor = this.id;
     userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
+
+    const audio = new Audio(`sounds/${userChosenColor}.mp3`);
+    audio.play();
   });
 });
